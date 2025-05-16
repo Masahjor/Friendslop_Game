3c2b1f5936b779682ab7bbf9208daf76c8368530
MacacoArts <Fortletrix@gmail.com>
2025-05-16 15:03:04 +0200

    Added extremely basic character controls (HE SLIDES LIKE A MOTHAFUCKAAAA!!!!)

---
+++ Assets/PlayerControls/PlayerCamera.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float SensX;
+    public float SensY;
+
+    public Transform Orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * SensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * SensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        Orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
